e5b7e93d53b98ae154f26a7f5d7fe78d0e221cde
xiechangqing <576535019@qq.com>
2017-10-24 17:18:20 +0800

    computed数据处理的三种方式，mapState

--- vuedemo/src/components/Count.vue
+++ vuedemo/src/components/Count.vue
@@ -2,7 +2,8 @@
   <div class="count">
     <h1>{{msg}}</h1>
     <hr>
-    <h3>{{$store.state.count}}</h3>
+    <h3>通过store获取：{{$store.state.count}}。</h3>
+    <h3>通过computed处理数据来获取(有三种获取方式)：{{count}}</h3>
     <button @click="$store.commit('add')">+</button>
     <button @click="$store.commit('reduce')">-</button>
   </div>
@@ -10,12 +11,25 @@
 
 <script>
   import store from '@/vuex/store';
+  import {mapState} from 'vuex';
   export default{
       data(){
           return{
               msg:"检测计数状态的改变,通过store的state来改变"
           }
       },
+//    方式1
+//      computed:{
+//        count(){
+//            return this.$store.state.count
+//        }
+//      },
+//    方式2 最常用的方法
+//    computed:mapState(['count']),
+//    方式3
+    computed:mapState({
+      count:state => state.count
+    }),
     store
   }
 </script>
