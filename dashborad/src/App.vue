d438a045ecad6b5f5ff0284db654c3a8656052b0
xiechangqing <576535019@qq.com>
2017-10-28 08:39:36 +0800

    接入echart

---
+++ dashborad/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <HeadNav></HeadNav>
+    <div class="content">
+      <router-view/>
+    </div>
+  </div>
+</template>
+
+<script>
+import HeadNav from '@/components/common/HeadNav';
+export default {
+  name: 'app',
+  components:{
+    HeadNav
+  }
+}
+</script>
+
+<style>
+  html,body{
+    margin: 0;
+    padding: 0;
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+  }
+  #app {
+    width: 100%;
+    height: 100%;
+  }
+  .content{
+    height: calc(100% - 50px);
+    background-color: skyblue;
+  }
+</style>
