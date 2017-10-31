d734e86a53cb9149bd51389ffd850785d5aff372
xiechangqing <576535019@qq.com>
2017-10-31 22:44:59 +0800

    feat:Bmap导入

--- telescope/src/components/page/performance.vue
+++ telescope/src/components/page/performance.vue
@@ -1,12 +1,12 @@
 <template>
-  <div>
-    <div>{{msg}}</div>
-    <!--<b-map-component></b-map-component>-->
+  <div class="fullBox pr">
+    <div id="map"></div>
+    <div id="orgSel">test</div>
   </div>
 </template>
 
 <script>
-
+  import BMap from 'BMap';
   export default{
       name:'performance',
       data(){
@@ -14,12 +14,10 @@
             msg:"performance"
           }
       },
-      mounted:function () {
-        var map = new BMap.Map("allmap");
-        map.centerAndZoom(new BMap.Point(116.404, 39.915), 11);  // 初始化地图,设置中心点坐标和地图级别
-        map.addControl(new BMap.MapTypeControl());   //添加地图类型控件
-        map.setCurrentCity("北京");          // 设置地图显示的城市 此项是必须设置的
-        map.enableScrollWheelZoom(true);
+      mounted() {
+        var map = new BMap.Map("map");
+        map.centerAndZoom(new BMap.Point(116.404, 39.915), 11);
+        map.setCurrentCity("北京");
       }
   }
 </script>
