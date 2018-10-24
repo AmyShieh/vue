5a439eb1007f3a4b34267db50058f5a9a9b08b3f
amyShieh <aliveamy719@gmail.com>
2018-10-24 16:38:46 +0800

    熟悉各类loader

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import _ from 'lodash'
+import './style.css';
+import bear from "./bear.jpeg";
+
 function component() {
   const element = document.getElementById("app")
-  element.innerHTML = _.join(["hello", "webpack"], ' ')
+  element.innerHTML = _.join(["hello", "webpack"], ' ');
+  element.classList.add("hello");
+
+  const myBear = new Image();
+  myBear.src = bear;
+  element.appendChild(myBear);
+
   return element
 }
 
