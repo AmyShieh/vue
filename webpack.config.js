96f045ab531485f7ca4296b4ef48659b5465a38d
amyShieh <aliveamy719@gmail.com>
2018-10-25 11:19:22 +0800

    add dev-tool

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,19 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
 
 module.exports = {
     entry: {
         app:"./src/index.js",
         print: "./src/print.js"
     },
+    devtool: 'inline-source-map',
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            title: 'output management'
+        })
+    ],
     output: {
         filename: '[name].main.js',
         path: path.resolve(__dirname, "dist")
