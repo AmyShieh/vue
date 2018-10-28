892211750946656eeb5b79e183559e9e2fd9f88c
Matthew5 <jnwu@thoughtworks.com>
2018-10-28 09:50:45 +0800

    Test format

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,13 +1,14 @@
 // export the function
 export function addNewHi() {
-  let newDiv = document.createElement("H1");
-  newDiv.setAttribute("class","hello-world");
-  newDiv.textContent = "hello webpack!";
-  console.log("utils2.js");
-  return newDiv;
+  let newDiv = document.createElement("H1")
+  newDiv.setAttribute("class", "hello-world")
+  // hehe
+  newDiv.textContent = "hello webpack!"
+  console.log("utils2.js")
+  return newDiv
 }
 
 // Define and export
-let lll = "<i class=\"da-font icon-dalanglogo\"></i>";
+let lll = '<i class="da-font icon-dalanglogo"></i>'
 
-export {lll};
+export { lll }
