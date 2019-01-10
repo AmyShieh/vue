e8c2c6cdb6886937b0fcb41879b802a6b8270854
xiechangqing <aliveAmy719@gmail.com>
2019-01-10 21:49:55 +0800

    update

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
 new Vue({
   el: '#app',
   data: {
-    name: 'Hello Amy!'
+    name: 'Amy',
+    age: 24,
+    website: "<a href='https://github.com/AmyShieh'>amy Shieh</a>"
+  },
+  methods: {
+    greeting: function (time) {
+      return `good ${time}, ${this.name} !`
+    },
+    addAge: function () {
+      this.age++;
+    }
+  },
+  computed: {
+    calculateTime: function() {
+      console.log("time");
+      return new Date()
+    },
+    calculateNum: function() {
+      console.log("num");
+      return this.age;
+    }
   }
 })
